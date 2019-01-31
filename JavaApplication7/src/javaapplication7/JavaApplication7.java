a7e59bd4309b518f1bacf36a3e64b3b8dbfaf870
Alumno <Alumno@a3profe>
2019-01-31 09:48:17 +0100

    prueba3.1

--- JavaApplication7/src/javaapplication7/JavaApplication7.java
+++ JavaApplication7/src/javaapplication7/JavaApplication7.java
@@ -15,7 +15,9 @@ public class JavaApplication7 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("hola");
+        if(true)
+            System.out.println("adios");
     }
     
 }
